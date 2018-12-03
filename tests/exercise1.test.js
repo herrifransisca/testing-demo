6c77bc68e4310222e088e5a4e96ae308701368ad
herrifransisca <herrifransisca@gmail.com>
2018-12-03 14:46:33 +0700

    15- Exercise- Testing the FizzBuz (mosh's code)

--- tests/exercise1.test.js
+++ tests/exercise1.test.js
@@ -1,43 +1,41 @@
-const exercise1 = require('../exercise1');
+const lib = require('../exercise1');
 
 describe('fizzBuzz', () => {
-  it('should throw an exception if input not a number', () => {
-    // expect(() => {
-    //   exercise1.fizzBuzz('a');
-    // }).toThrow();
-
-    const args = [null, undefined, '', false];
-
-    args.forEach(a => {
-      expect(() => {
-        exercise1.fizzBuzz(a);
-      }).toThrow();
-    });
-  });
-
-  it('should throw an exception if input is a string', () => {
+  it('should throw an exception if input is not a number', () => {
     expect(() => {
-      exercise1.fizzBuzz('test');
+      lib.fizzBuzz('a');
+    }).toThrow();
+
+    expect(() => {
+      lib.fizzBuzz(null);
+    }).toThrow();
+
+    expect(() => {
+      lib.fizzBuzz(undefined);
+    }).toThrow();
+
+    expect(() => {
+      lib.fizzBuzz({});
     }).toThrow();
   });
 
-  it('should return Fizz if input can be divided by 3', () => {
-    const result = exercise1.fizzBuzz(3);
-    expect(result).toBe('Fizz');
-  });
-
-  it('Should return Buzz if input can be divided by 5', () => {
-    const result = exercise1.fizzBuzz(5);
-    expect(result).toBe('Buzz');
-  });
-
-  it('should return FizzBuzz if input can be both divided by 3 and 5', () => {
-    const result = exercise1.fizzBuzz(15);
+  it('should return FizzBuzz if input is divisible by 3 and 5', () => {
+    const result = lib.fizzBuzz(15);
     expect(result).toBe('FizzBuzz');
   });
 
-  it('should return input value if input cannot be divided by 5 and 3, or by 5, or by 3', () => {
-    const result = exercise1.fizzBuzz(7);
-    expect(result).toBe(7);
+  it('should return Fizz if input is only divisible by 3', () => {
+    const result = lib.fizzBuzz(3);
+    expect(result).toBe('Fizz');
+  });
+
+  it('Should return Buzz if input is only divisible by 5', () => {
+    const result = lib.fizzBuzz(5);
+    expect(result).toBe('Buzz');
+  });
+
+  it('should return input if it is not divisible by 3 or 5', () => {
+    const result = lib.fizzBuzz(1);
+    expect(result).toBe(1);
   });
 });
