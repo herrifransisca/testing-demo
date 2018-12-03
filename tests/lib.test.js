cde27d1a468aede44547b22daef8f62975803316
herrifransisca <herrifransisca@gmail.com>
2018-12-03 16:06:37 +0700

    12-16- Creating Simple Mock Functions

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,4 +1,5 @@
 const lib = require('../lib');
+const db = require('../db');
 
 describe('absolute', () => {
   it('should return a positive number if input is positive', () => {
@@ -98,5 +99,18 @@ describe('registerUser', () => {
     const result = lib.registerUser('herri');
     expect(result).toMatchObject({ username: 'herri' });
     expect(result.id).toBeGreaterThan(0);
+  });
+});
+
+describe('applyDiscount', () => {
+  it('should apply 10% discount if customer has more than 10 points', () => {
+    db.getCustomerSync = function(customerId) {
+      console.log('Fake reading customer...');
+      return { id: customerId, points: 20 };
+    };
+
+    const order = { customerId: 1, totalPrice: 10 };
+    lib.applyDiscount(order);
+    expect(order.totalPrice).toBe(9);
   });
 });
